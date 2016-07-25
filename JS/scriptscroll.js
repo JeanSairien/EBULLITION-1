218bdcdd9c756b9e3c3a1cdabfdd9349151ecb8c
Yose <youness;chetoui@gmail.com>
2016-07-25 16:12:14 +0200

    ajout du responsive

--- JS/scriptscroll.js
+++ JS/scriptscroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){       
    var scroll_start = 0;
-   var startchange = $('#header');
+   var startchange = $('#Presentation');
    var offset = startchange.offset();
     if (startchange.length){
    $(document).scroll(function() { 
